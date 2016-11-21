e081c392f367c47793b153bbe7c12979d85a91f8
Juanfa <juanfpo96@gmail.com>
2016-11-21 17:40:48 +0100

    asd

---
+++ app/src/main/java/com/plataformas/modelos/escenarios/Caja.java
@@ -0,0 +1,40 @@
+package com.plataformas.modelos.escenarios;
+
+import android.content.Context;
+import android.graphics.Canvas;
+
+import com.plataformas.R;
+import com.plataformas.gestores.CargadorGraficos;
+import com.plataformas.modelos.Modelo;
+
+/**
+ * Created by uo227602 on 15/11/2016.
+ */
+
+public class Caja extends Modelo {
+
+    double velocidadX = 3;
+
+    public Caja(Context context, double x, double y) {
+        super(context, x, y, 32, 40);
+
+        this.y = y -altura/2;
+
+
+        imagen = CargadorGraficos.cargarDrawable(context, R.drawable.platform);
+    }
+
+    public boolean girar(){
+        velocidadX = velocidadX*-1;
+        return true;
+    }
+
+    public void dibujar(Canvas canvas){
+        int yArriva = (int)  y - altura/2  - Nivel.scrollEjeY;
+        int xIzquierda = (int) x - ancho/2 - Nivel.scrollEjeX;
+
+        imagen.setBounds(xIzquierda, yArriva, xIzquierda
+                + ancho, yArriva + altura);
+        imagen.draw(canvas);
+    }
+}
